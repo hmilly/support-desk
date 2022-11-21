159e73e6d3d266adf79ecc8b28eaf588ca89b4fa
hmilly <milstowers@gmail.com>
2022-11-21 21:04:00 +0000

    added backend folders inc middlwears &new packages

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,56 @@
-const registeredUser = (req, res) => {
-  res.send("Register Route");
-};
+const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
+const User = require("../models/userModel");
 
-const loginUser = (req, res) => {
+// Register a new user
+//  /api/users
+//  Public
+
+const registeredUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  // throw err if field is missing
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please include all fields");
+  }
+
+  // exit if user exists
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  // hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPword = await bcrypt.hash(password, salt);
+
+  // create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPword,
+  });
+
+  if (user) {
+    res.status(201).send({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new error("Invalid user data");
+  }
+});
+
+// login a new user
+// /api/users/login
+// Public
+const loginUser = asyncHandler(async (req, res) => {
   res.send("login Route");
-};
+});
 
 module.exports = {
   registeredUser,
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const colors = require('colors')
+const connectDb = require('./config/db')
+const { errorHandler } = require("./middlewear/errorMiddlewear");
 const PORT = process.env.PORT || 8000;
 const app = express();
+
+connectDb()
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to the support desk API" });
@@ -13,5 +21,6 @@ app.get("/users", (req, res) => {
 
 // Routes
 app.use("/api/users", require("./routes/userRoutes"));
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
