3366381de26485091856e625794a52e5aae93b40
hmilly <milstowers@gmail.com>
2022-11-29 18:02:18 +0000

    updating frontend code

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const dotenv = require("dotenv").config();
-const colors = require('colors')
-const connectDb = require('./config/db')
+require("colors");
+require("dotenv").config();
+const connectDb = require("./config/db");
 const { errorHandler } = require("./middlewear/errorMiddlewear");
-const PORT = process.env.PORT || 8000;
-const app = express();
+const PORT = process.env.PORT || 5000;
 
-connectDb()
+const app = express();
+connectDb();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,4 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import authService from "./authService";
 
 const initialState = {
   user: null,
@@ -11,7 +12,18 @@ const initialState = {
 export const register = createAsyncThunk(
   "auth/register",
   async (user, thunkAPI) => {
-    console.log(user);
+    try {
+      return await authService.register();
+    } catch (error) {
+      const msg =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+
+      return thunkAPI.rejectWithValue(msg);
+    }
   }
 );
 
@@ -22,8 +34,32 @@ export const login = createAsyncThunk("auth/login", async (user, thunkAPI) => {
 export const authSlice = createSlice({
   name: "auth",
   initialState,
-  reducers: {},
-  extraReducers: (builder) => {},
+  reducers: {
+    reset: (state) => {
+      state.isLoading = false;
+      state.isError = false;
+      state.isSuccess = false;
+      state.message = "";
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(register.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = action.payload;
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.isLoading = false;
+        state.user = null;
+        state.isError = true;
+        state.message = action.payload;
+      });
+  },
 });
 
+export const { reset } = authSlice.actions;
 export default authSlice.reducer;
