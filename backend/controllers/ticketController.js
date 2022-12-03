d2cbc2ab540a39e2886df87525b599b7c25dc3e4
hmilly <milstowers@gmail.com>
2022-12-03 16:08:27 +0000

    adding ticket functionality

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -5,18 +5,42 @@ const Ticket = require("../models/ticketModel");
 // api/tickets
 // GET user tickets
 const getTickets = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: "get tickets" });
-  });
+  // get user using id and JWT
+  const user = await User.findById(req.user.id);
 
-  
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found!");
+  }
+  const tickets = await Ticket.find({ user: req.user.id });
+  res.status(200).json(tickets);
+});
+
 // api/tickets/
 // POST create new ticket
 const createTicket = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: "create tickets" });
-  });
-  
-
-  module.exports = {
-    getTickets,
-    createTicket
+  const { product, description } = req.body;
+  if (!product || !description) {
+    res.status(401);
+    throw new Error("Please add a product and description");
   }
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found!");
+  }
+
+  const ticket = await Ticket.create({
+    product,
+    description,
+    user: req.user.id,
+    status: "new",
+  });
+  res.status(201).json(ticket);
+});
+
+module.exports = {
+  getTickets,
+  createTicket,
+};
