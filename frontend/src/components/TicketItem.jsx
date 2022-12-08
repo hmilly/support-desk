0eb36cffe61df769d536b378862df03a83fae0aa
hmilly <milstowers@gmail.com>
2022-12-08 16:30:48 +0000

    added note item in frontend & displayed in tickets

--- frontend/src/components/TicketItem.jsx
+++ frontend/src/components/TicketItem.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const TicketItem = ({ ticket }) => {
   return (
     <div className="ticket">
-      <div>{new Date(ticket.createdAt).toLocaleString("en-GB")}</div>
+      <div>{new Date(ticket.createdAt).toLocaleString("en-GB").slice(0, -3)}</div>
       <div>{ticket.product}</div>
       <div className={`status status-${ticket.status}`}>{ticket.status}</div>
       <Link to={`/ticket/${ticket._id}`} className="btn btn-reverse btn-sm">
--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,15 +1,21 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams, useNavigate } from "react-router-dom";
-import { getTicket, reset, closeTicket } from "../features/tickets/ticketSlice";
+import { getTicket, closeTicket } from "../features/tickets/ticketSlice";
+import { getNotes, reset as notesReset } from "../features/notes/noteSlice";
 import { toast } from "react-toastify";
 import Spinner from "../components/Spinner";
 import BackButton from "../components/BackButton";
+import NoteItem from "../components/NoteItem";
 
 const Ticket = () => {
   const { ticket, isLoading, isError, isSuccess, message } = useSelector(
     (state) => state.tickets
   );
+  const { notes, isLoading: notesIsLoading } = useSelector(
+    (state) => state.notes
+  );
+
   const dispatch = useDispatch();
   const params = useParams();
   const navigate = useNavigate();
@@ -20,6 +26,7 @@ const Ticket = () => {
       toast.error(message);
     }
     dispatch(getTicket(ticketId));
+    dispatch(getNotes(ticketId));
   }, [isError, message, dispatch, ticketId]);
 
   const onTicketClose = () => {
@@ -28,7 +35,7 @@ const Ticket = () => {
     navigate("/tickets");
   };
 
-  if (isLoading) {
+  if (isLoading || notesIsLoading) {
     return <Spinner />;
   }
   if (isError) {
@@ -45,7 +52,8 @@ const Ticket = () => {
           </span>
         </h2>
         <h3>
-          Date submitted: {new Date(ticket.createdAt).toLocaleString("en-GB")}
+          Date submitted:{" "}
+          {new Date(ticket.createdAt).toLocaleString("en-GB").slice(0, -3)}
         </h3>
         <h3>Product: {ticket.product}</h3>
         <hr />
@@ -53,7 +61,12 @@ const Ticket = () => {
           <h3>Description of Issue</h3>
           <p>{ticket.description}</p>
         </div>
+        <h2>Notes</h2>
       </header>
+
+      {notes.map((note) => (
+        <NoteItem key={note._id} note={note} />
+      ))}
 
       {ticket.status !== "closed" ? (
         <button className="btn btn-block btn-danger" onClick={onTicketClose}>
